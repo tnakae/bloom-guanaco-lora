cef24e2daeae5691cf868f842bb3106a99538d11
Toshihiro NAKAE <35999080+tnakae@users.noreply.github.com>
2023-03-24 23:58:41 +0900

    Added generator to BloomLoRa model class (#3)
    * Added generator to BloomLoRa class
    * Updated based on pre-commit results

--- bloom-guanaco-lora/app/bloom_lora.py
+++ bloom-guanaco-lora/app/bloom_lora.py
@@ -1,65 +1,65 @@
-import os
 import sys
-from typing import Dict, Union
+from typing import Dict, Optional
 
 import bitsandbytes as bnb  # noqa
 import torch
 import transformers
-from datasets import (
-    load_dataset,
-    DatasetDict,
-)
-from transformers import (
-    BloomTokenizerFast,
-    BloomForCausalLM,
-)
+from datasets import DatasetDict, load_dataset
 from peft import (
-    prepare_model_for_int8_training,
     LoraConfig,
     get_peft_model,
     get_peft_model_state_dict,
+    prepare_model_for_int8_training,
 )
+from transformers import BloomForCausalLM, BloomTokenizerFast, GenerationConfig
 
 from .config import ModelConfig
 from .prompt import PromptGenerator
 
 
 class BloomLoRa:
-    def __init__(self, config: ModelConfig):
-        device_map: Union[str, Dict[str, int]] = "auto"
-        world_size = int(os.environ.get("WORLD_SIZE", 1))
+    def __init__(self, config: ModelConfig, training: bool = True):
+        device_map: str = "auto"
 
         self.config = config
-        self.config.llm.world_size = world_size
-        self.ddp = world_size != 1
-
-        if self.ddp:
-            device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
+        if torch.cuda.is_available():
+            self.device = "cuda"
+        else:
+            self.device = "cpu"
 
         self.model = BloomForCausalLM.from_pretrained(
             config.training.model_name,
-            load_in_8bit=config.training.load_in_8bit,
+            load_in_8bit=True,
+            torch_dtype=torch.float16,
             device_map=device_map,
         )
         self.tokenizer = BloomTokenizerFast.from_pretrained(
-            config.training.model_name,
-            add_eos_token=True
+            config.training.model_name, add_eos_token=True
         )
 
-        self.model = prepare_model_for_int8_training(self.model)
-        self.model = get_peft_model(self.model,
-                                    LoraConfig(**config.lora.dict()))
+        if training:
+            self.model = prepare_model_for_int8_training(self.model)
+            self.model = get_peft_model(self.model, LoraConfig(**config.lora.dict()))
 
-        # unk. we want this to be different from the eos token
-        self.tokenizer.pad_token_id = 0
+            # unk. we want this to be different from the eos token
+            self.tokenizer.pad_token_id = 0
+            self.data: DatasetDict = load_dataset(
+                "json", data_files=config.file_path.data_path
+            )
 
-        self.data: DatasetDict = load_dataset("json",
-                                              data_files=config.file_path.data_path)
+            self.prompt_generator = PromptGenerator(
+                self.tokenizer, config.llm.cutoff_len
+            )
+        else:
+            self.model = get_peft_model(
+                self.model, config.file_path.model_dir, torch_dtype=torch.float16
+            )
 
-        self.prompt_generator = PromptGenerator(self.tokenizer,
-                                                config.llm.cutoff_len)
+    @classmethod
+    def from_finetuned(cls, config: ModelConfig) -> "BloomLoRa":
+        return BloomLoRa(config, training=False)
 
-    def fine_tuning(self):
+    def fine_tuning(self) -> None:
         generate_prompt = self.prompt_generator.get_generate_method()
         if self.config.training.val_set_size > 0:
             train_val = self.data["train"].train_test_split(
@@ -68,7 +68,7 @@ class BloomLoRa:
             train_data = train_val["train"].shuffle().map(generate_prompt)
             val_data = train_val["test"].shuffle().map(generate_prompt)
         else:
-            train_data = self.data['train'].shuffle().map(generate_prompt)
+            train_data = self.data["train"].shuffle().map(generate_prompt)
             val_data = None
 
         trainer = transformers.Trainer(
@@ -83,16 +83,21 @@ class BloomLoRa:
                 learning_rate=self.config.llm.learning_rate,
                 fp16=True,
                 logging_steps=20,
-                evaluation_strategy="steps" if self.config.training.val_set_size > 0 else "no",
+                evaluation_strategy="steps"
+                if self.config.training.val_set_size > 0
+                else "no",
                 save_strategy="steps",
                 eval_steps=200 if self.config.training.val_set_size > 0 else None,
                 save_steps=200,
-                output_dir=self.config.file_path.output_dir,
+                output_dir=self.config.file_path.model_dir,
                 save_total_limit=3,
-                load_best_model_at_end=True if self.config.training.val_set_size > 0 else False,
-                ddp_find_unused_parameters=False if self.ddp else None,
+                load_best_model_at_end=True
+                if self.config.training.val_set_size > 0
+                else False,
             ),
-            data_collator=transformers.DataCollatorForLanguageModeling(self.tokenizer, mlm=False),
+            data_collator=transformers.DataCollatorForLanguageModeling(
+                self.tokenizer, mlm=False
+            ),
         )
         self.model.config.use_cache = False
 
@@ -101,9 +106,28 @@ class BloomLoRa:
             lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
         ).__get__(self.model, type(self.model))
 
-        if torch.__version__ >= "2" and sys.platform != 'win32':
-            model = torch.compile(self.model)
+        if torch.__version__ >= "2" and sys.platform != "win32":
+            self.model = torch.compile(self.model)
 
         trainer.train()
-        model.save_pretrained(self.config.file_path.output_dir)
+        self.model.save_pretrained(self.config.file_path.model_dir)
         print("\n If there's a warning about missing keys above, please disregard :)")
+
+    def evaluate(self, instruction: str, input: Optional[str] = None) -> str:
+        prompt_params: Dict[str, str] = {"instruction": instruction}
+        if input is not None:
+            prompt_params["input"] = input
+        prompt = self.prompt_generator.generate(prompt_params)
+
+        generation_config = GenerationConfig(**self.config.generation.dict())
+        with torch.no_grad():
+            generation_output = self.model.generate(
+                input_ids=prompt["input_ids"].to(self.device),
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+                max_new_tokens=self.config.generation.max_new_tokens,
+            )
+        s = generation_output.sequences[0]
+        output: str = self.tokenizer.decode(s)
+        return output.split("### Response:")[1].strip()
--- bloom-guanaco-lora/app/prompt.py
+++ bloom-guanaco-lora/app/prompt.py
@@ -4,13 +4,13 @@ from transformers import BloomTokenizerFast
 
 
 class PromptGenerator:
-    def __init__(self,
-                 tokenizer: BloomTokenizerFast,
-                 cutoff_len: int):
+    def __init__(self, tokenizer: BloomTokenizerFast, cutoff_len: int):
         self.tokenizer = tokenizer
         self.cutoff_len = cutoff_len
 
-    def generate(self, data_point: Dict[str, str]) -> Dict[str, Any]:
+    def generate(
+        self, data_point: Dict[str, str], training: bool = True
+    ) -> Dict[str, Any]:
         # This function masks out the labels for the input,
         # so that our loss is computed only on the response.
         user_prompt = (
@@ -24,7 +24,8 @@ class PromptGenerator:
                 f"{data_point['input']}\n\n"
                 "### Response:"
             )
-            if data_point["input"] else (
+            if data_point["input"]
+            else (
                 "Below is an instruction that describes a task. "
                 "Write a response that appropriately completes the request.\n\n"
                 "### Instruction:\n"
@@ -33,32 +34,37 @@ class PromptGenerator:
             )
         )
 
-        len_user_prompt_tokens = (
-            len(
-                self.tokenizer(
-                    user_prompt,
-                    truncation=True,
-                    max_length=self.cutoff_len + 1,
-                )["input_ids"]
-            )
-            - 1
-        )  # no eos token
+        if training:
+            len_user_prompt_tokens = (
+                len(
+                    self.tokenizer(
+                        user_prompt,
+                        truncation=True,
+                        max_length=self.cutoff_len + 1,
+                    )["input_ids"]
+                )
+                - 1
+            )  # no eos token
 
-        full_tokens = self.tokenizer(
-            user_prompt + data_point["output"],
-            truncation=True,
-            max_length=self.cutoff_len + 1,
-            padding="max_length",
-        )["input_ids"][:-1]
+            full_tokens = self.tokenizer(
+                user_prompt + data_point["output"],
+                truncation=True,
+                max_length=self.cutoff_len + 1,
+                padding="max_length",
+            )["input_ids"][:-1]
 
-        return {
-            "input_ids": full_tokens,
-            "labels": [-100] * len_user_prompt_tokens
-            + full_tokens[len_user_prompt_tokens:],
-            "attention_mask": [1] * (len(full_tokens)),
-        }
+            return {
+                "input_ids": full_tokens,
+                "labels": [-100] * len_user_prompt_tokens
+                + full_tokens[len_user_prompt_tokens:],
+                "attention_mask": [1] * (len(full_tokens)),
+            }
+        else:
+            inputs = self.tokenizer(user_prompt, return_tensors="pt")
+            return {"input_ids": inputs["input_ids"]}
 
     def get_generate_method(self) -> Callable[[Dict[str, str]], Dict[str, Any]]:
-        def generator_method(data_point):
+        def generator_method(data_point: Dict[str, str]) -> Dict[str, Any]:
             return self.generate(data_point)
+
         return generator_method
